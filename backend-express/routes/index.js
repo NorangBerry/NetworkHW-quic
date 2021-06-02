a0b3478e697a8977c034cfc7aeb249f16bdecec1
NorangBerry <hlnsud12@naver.com>
2021-06-03 07:10:07 +0900

    [FEAT]send image on node

--- backend-express/routes/index.js
+++ backend-express/routes/index.js
@@ -1,21 +1,59 @@
 const server_http2 = require('./http2')
 const server_http3 = require('./quic')
 const fs = require('fs');
+const mime = require("mime");
 
 const html = fs.readFileSync(__dirname + '/../sample.html','utf8');
+const image = fs.createReadStream(__dirname + '/../sample.jpg');
 
-server_http2.on('stream', (stream, headers) => {
-  // stream is a Duplex
-  stream.respond({
-    'content-type': 'text/html; charset=utf-8',
-    ':status': 200,
-		'Access-Control-Allow-Origin': '*'
+
+const sendFile = (stream, fileName) => {
+  var s = fs.createReadStream(fileName);
+  s.on('open', function () {
+      stream.respond({
+        'Content-Type': 'image/jpeg',
+        ':status': 200,
+        'Access-Control-Allow-Origin': '*'
+      });
+      s.pipe(stream);
   });
-  stream.end(html);
+};
+
+server_http2.on('stream',async (stream, headers) => {
+  const path = headers[':path']
+  console.log(path)
+  if(path==="/"){
+    stream.respond({
+      'Content-Type': 'text/html; charset=utf-8',
+      ':status': 200,
+      'Access-Control-Allow-Origin': '*'
+    });
+    stream.end(html);
+  }
+  else if(path==="/image"){
+    sendFile(stream,__dirname + '/../sample.jpg')
+  }
 });
 
 server_http3.on('session', async (session) => {
+  // console.log(session)
   session.on('stream', (stream) => {
-    stream.end(html)
+    stream.setEncoding('utf8');
+    var req = ''
+    stream.on('data', (data)=>{
+      req += data
+    });
+    stream.on('end', async (data)=>{
+      const path = req.split('\n')[0].split(' ')[1]
+      if(path === '/'){
+        stream.end(html)
+      }
+      else if(path == '/image'){
+        var s = fs.createReadStream(__dirname + '/../sample.jpg');
+        s.on('open', function () {
+            s.pipe(stream);
+        });
+      }
+    });
   });
 });
--- front-node/client.js
+++ front-node/client.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const https = require('https');
 const http2 = require('http2');
 const req_header = fs.readFileSync(__dirname + '/sample_http_header.txt','utf8');
+const req_header_image = fs.readFileSync(__dirname + '/sample_http_header_image.txt','utf8');
 
 const socket = createQuicSocket();
 // const URL = '192.168.0.2'
@@ -12,7 +13,7 @@ const PORT3 = 9001
 
 HTTP2_URLS=['https://172.17.90.8:9000']
 HTTP3_URLS=['https://172.17.90.8:9001']
-PATHS = ['/']
+PATHS = ['/','/image']
 
 async function main(){
 	await test_tcp()
@@ -22,14 +23,14 @@ async function test_tcp(){
 	const ca = fs.readFileSync(__dirname + "/public.pem")
 	HTTP2_URLS.forEach(url =>{
 		PATHS.forEach(path=>{
-			const req_path = `${url}${path}`
-			const client = http2.connect(req_path, {
+			const client = http2.connect(url, {
 				ca: ca,
 				rejectUnauthorized: false
 			});
 			const req = client.request({
 				[http2.constants.HTTP2_HEADER_SCHEME]: "https",
-				[http2.constants.HTTP2_HEADER_METHOD]: http2.constants.HTTP2_METHOD_GET,
+				[http2.constants.HTTP2_HEADER_METHOD]: http2.constants.HTTP2_METHOD_POST,
+				[http2.constants.HTTP2_HEADER_PATH]: path,
 			});
 			req.on('data', (chunk) => {
 			});
@@ -37,7 +38,7 @@ async function test_tcp(){
 			req.end();
 			req.on('end', () => {
 				const diff = new Date().getTime() - before.getTime()
-				console.log(`http2: ${diff}`)
+				console.log(`http2: ${path} : ${diff}`)
 			});
 		})
 	})
@@ -48,26 +49,21 @@ async function test_quic(){
 			const req_path = new URL(`${url}${path}`)
 			const client = await socket.connect({address:req_path.hostname,port:req_path.port,alpn:'h3'})
 			const stream = await client.openStream();
+			stream.setEncoding('utf8');
 			stream.on('data', (data) => {
 			});
+			var req = req_header_image
+			if(path === '/'){
+				req = req_header
+			}
 			const before = new Date()
-			stream.end(req_header);
+			stream.end(req);
 			stream.on('end',() =>{
 				const diff = new Date().getTime() - before.getTime()
-				console.log(`http3: ${diff}`)
+				console.log(`http3: ${path} : ${diff}`)
 			})
 		});
 	});
-	// const client = await socket.connect({address:URL,port:PORT3,alpn:'h3'})
-	// const stream = await client.openStream();
-	// stream.on('data', (data) => {
-	// });
-	// const before = new Date()
-	// stream.end(req_header);
-	// stream.on('end',() =>{
-	// 	const diff = new Date().getTime() - before.getTime()
-	// 	console.log(`http3: ${diff}`)
-	// })
 }
 
 main()
